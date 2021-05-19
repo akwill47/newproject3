4a6cc281dc33437f776701016748bac1eb3b835f
Akwill47 <wil.weat@hotmail.com>
2021-05-19 15:26:26 -0400

    full update

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -15,6 +13,9 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue();
+    }
+
+    private void assertTrue() {
     }
 }
